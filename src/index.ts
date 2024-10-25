7de0679b9a1c0b527613cd83069af80a30e49ecd
imba97 <mail@imba97.cn>
2024-10-25 21:48:35 +0800

    chore: change argument name

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { InitxHandler, type InitxOptions } from '@initx-plugin/core'
+import { type InitxCtx, InitxHandler } from '@initx-plugin/core'
 import { log } from '@initx-plugin/utils'
 
 export default class StarterHandler extends InitxHandler {
@@ -9,11 +9,11 @@ export default class StarterHandler extends InitxHandler {
     }
   ]
 
-  async handle(options: InitxOptions, ...others: string[]) {
+  async handle(ctx: InitxCtx, ...others: string[]) {
     log.info('initx-plugin-starter is running 🎊')
 
-    log.info('options')
-    console.log(options)
+    log.info('ctx')
+    console.log(ctx)
 
     log.info('others')
     console.log(others)
